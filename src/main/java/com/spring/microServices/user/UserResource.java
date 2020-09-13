648cc18f46e699b1c9f5b0ccc9da685fec4f3b03
Veronica <valdiviavv@hotmail.com>
2020-09-13 14:29:47 -0400

    the exception classes have been created.

--- src/main/java/com/spring/microServices/user/UserResource.java
+++ src/main/java/com/spring/microServices/user/UserResource.java
@@ -22,7 +22,10 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+        return user;
     }
 
     @PostMapping("/users")
