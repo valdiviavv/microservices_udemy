49ef247716e53c0671f9e1b3a992c8edafe896ec
Veronica <valdiviavv@hotmail.com>
2020-09-13 15:16:12 -0400

    the validation for the name have been created.

--- src/main/java/com/spring/microServices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/spring/microServices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.spring.microServices.exception;
 
 import com.spring.microServices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,15 @@ public class CustomizedResponseEntityExceptionHandler extends
                         request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+              HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), "Validation failed",
+                        ex.getBindingResult().toString());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/com/spring/microServices/user/UserResource.java
+++ src/main/java/com/spring/microServices/user/UserResource.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -28,8 +29,16 @@ public class UserResource {
         return user;
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+    }
+
+
     @PostMapping("/users")
-    public ResponseEntity<Object>  createUser(@RequestBody User user) {
+    public ResponseEntity<Object>  createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder
