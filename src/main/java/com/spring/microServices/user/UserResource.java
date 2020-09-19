f3bbc36cc1cf81d6e91f8d5eb61e9966a8e9f242
Veronica <valdiviavv@hotmail.com>
2020-09-19 12:41:37 -0400

    Added the hateoas for the retrieveUser method.

--- src/main/java/com/spring/microServices/user/UserResource.java
+++ src/main/java/com/spring/microServices/user/UserResource.java
@@ -1,5 +1,10 @@
 package com.spring.microServices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +27,28 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
-        if(user == null)
-            throw new UserNotFoundException("id-" + id);
-        return user;
+
+        if(user==null)
+            throw new UserNotFoundException("id-"+ id);
+
+
+        //"all-users", SERVER_PATH + "/users"
+        //retrieveAllUsers
+        EntityModel<User> resource = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo =
+                linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+
+        //HATEOAS
+
+        return resource;
     }
+//
+
 
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id) {
