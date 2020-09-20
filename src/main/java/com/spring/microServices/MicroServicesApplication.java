cbe71f1f4a50bc7574f070e79cad1dc3139379f8
Veronica <valdiviavv@hotmail.com>
2020-09-20 15:17:39 -0400

    The SwaggerConfig has been set up and it's working fine.
    The link to see the SwaggerConfig is
    http://localhost:8080/swagger-ui/index.html

--- src/main/java/com/spring/microServices/MicroServicesApplication.java
+++ src/main/java/com/spring/microServices/MicroServicesApplication.java
@@ -2,6 +2,7 @@ package com.spring.microServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
