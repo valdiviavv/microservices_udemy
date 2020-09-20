b83e3103425307be44d117375703e0c5de3ea0a1
Veronica <valdiviavv@hotmail.com>
2020-09-20 13:41:50 -0400

    Made the helloWorldInternationalized method shorter.

--- src/main/java/com/spring/microServices/helloWorld/HelloWorldController.java
+++ src/main/java/com/spring/microServices/helloWorld/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.spring.microServices.helloWorld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,8 +33,9 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null,
+                LocaleContextHolder.getLocale());
     }
 
 }
