c47187f397827b88a16d88fb70cd58a1ef3ae0ae
Veronica <valdiviavv@hotmail.com>
2020-09-27 13:43:53 -0400

    The process to populate the H2 database was started.

--- src/main/java/com/spring/microServices/user/User.java
+++ src/main/java/com/spring/microServices/user/User.java
@@ -3,13 +3,19 @@ package com.spring.microServices.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @ApiModel(description = "Details about the user.")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name should have at least two characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(1,sysdate(), 'AB');
+insert into user values(2,sysdate(), 'Jill');
+insert into user values(3,sysdate(), 'Jam ');
+
